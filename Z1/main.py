6a6de9bc9ea9ef2f7e6fa7a87b7d2c8c25722a26
milospp <misa01523@gmail.com>
2022-03-27 12:44:48 +0200

    Changed model to log

--- Z1/main.py
+++ Z1/main.py
@@ -5,6 +5,7 @@ import sys
 # import matplotlib.pyplot as plt
 
 
+# e = 1.718281828459045
 class Regression:
 
     def __init__(self):
@@ -13,31 +14,41 @@ class Regression:
 
     def calc_function(self, x):
         # t1 * x^5 + t2
-        return self._t0 * x**6 + self._t1
+        return np.power(10, (self._t0 * x + self._t1))
+        # return self._t0 * x**8 + x**e + self._t1
 
-    def derivative_t0(self, x, y, y_predicted):
-        return -(2 / len(x)) * sum(6 * x * (y - y_predicted))
+    def calc_log_function(self, x):
+        # t1 * x^5 + t2
+        return self._t0 * x + self._t1
 
-    def derivative_t1(self, y, y_predicted):
-        return -(2 / len(y)) * sum(y - y_predicted)
+    def derivative_t0(self, x, y_predicted, y):
+        return (2 / len(x)) * sum((y_predicted - y) * (x))
 
-    def mean_squared_error(self, y_true, y_predicted):
+    def derivative_t1(self, x, y_predicted, y):
+        return (2 / len(x)) * sum((y_predicted - y) * (1))
+
+    def mean_squared_error(self, y_predicted, y_true):
         # Calculating the loss or cost
-        cost = np.sum((y_true - y_predicted) ** 2) / len(y_true)
+        cost = np.sum((y_predicted - y_true) ** 2) / len(y_true)
         return cost
 
-    def root_mean_squared_error(self, y_true, y_predicted):
-        return self.mean_squared_error(y_true, y_predicted) ** 0.5
+    def root_mean_squared_error(self, y_predicted, y_true):
+        return self.mean_squared_error(y_predicted, y_true) ** 0.5
 
     # def plot_function(self, from_x, to_x):
     #     x_data = np.linspace(from_x, to_x, 100)
-    #     y_data = reg.calc_function(x_data)
+    #     y_data = self.calc_function(x_data)
     #     plt.plot(x_data, y_data)
 
-    def gradient_descent(self, x, y, iterations=12000, learning_rate=0.2,
-                         stopping_threshold=1e-6, validation_data=None):
-        self._t0 = 0.1
-        self._t1 = 0.01
+    def gradient_descent(self, x, y, iterations=20000, learning_rate=0.01,
+                         stopping_threshold=1e-8, validation_data=None):
+
+        y = np.log10(y)
+
+        self._t0 = 7.8339
+        self._t1 = -1.7869
+        # self._t0 = 0
+        # self._t1 = 0
 
         validation_best_t0 = self._t0
         validation_best_t1 = self._t1
@@ -51,16 +62,16 @@ class Regression:
 
         for i in range(iterations):
 
-            y_predicted = self.calc_function(x)
+            y_predicted = self.calc_log_function(x)
 
-            current_cost = self.root_mean_squared_error(y, y_predicted)
+            current_cost = self.root_mean_squared_error(y_predicted, y)
 
             if previous_cost and abs(previous_cost - current_cost) <= stopping_threshold:
                 break
 
             if validation_data is not None:
-                valid_y_predicted = self.calc_function(validation_data.X)
-                validation_current_cost = self.root_mean_squared_error(validation_data.Y, valid_y_predicted)
+                valid_y_predicted = self.calc_log_function(validation_data.X)
+                validation_current_cost = self.root_mean_squared_error(valid_y_predicted, validation_data.Y)
 
                 if validation_current_cost < validation_best_cost:
                     validation_best_cost = validation_current_cost
@@ -73,28 +84,25 @@ class Regression:
             weights.append(self._t0)
 
             # Calculating the gradients
-            t0_derivative = self.derivative_t0(x, y, y_predicted)
-            t1_derivative = self.derivative_t1(y, y_predicted)
+            t0_derivative = self.derivative_t0(x, y_predicted, y)
+            t1_derivative = self.derivative_t1(x, y_predicted, y)
 
             # Updating weights and bias
             self._t0 = self._t0 - (learning_rate * t0_derivative)
             self._t1 = self._t1 - (learning_rate * t1_derivative)
 
             # Printing the parameters for each 1000th iteration
-            # print(f"Iteration {i + 1}: Cost: {current_cost:<10.5} t0: {self._t0:<10.5} t1: {self._t1:<10.5}")
+            print(f"Iteration {i + 1}: Cost: {10**current_cost:<10.5} t0: {self._t0:<10.5} t1: {self._t1:<10.5}")
 
         # Visualizing the weights and cost at for all iterations
-        # plt.figure(figsize=(8, 6))
         # plt.plot(weights, costs)
-        # plt.scatter(weights, costs, marker='o', color='red')
-        # plt.title("Cost vs Weights")
+        # plt.title("Gradient descent")
         # plt.ylabel("Cost")
-        # plt.xlabel("Weight")
-        # plt.show()
+        # plt.xlabel("T0")
         # print("Current cost: " + str(current_cost))
 
         if validation_data is not None:
-            print("Valid-C-Cost:", validation_current_cost, " | Valid-Best-Cost", validation_best_cost )
+            # print("Valid-C-Cost:", validation_current_cost, " | Valid-Best-Cost", validation_best_cost )
             self._t0 = validation_best_t0
             self._t1 = validation_best_t1
 
@@ -104,15 +112,15 @@ class Regression:
 def remove_outliers(csv):
     outliers = []
     for i, [x, y] in enumerate(csv.values):
-        if 0 < x < 1 and y > 30:
+        if 0 < x < 0.4 and y > 30:
             outliers.append(i)
 
     csv.drop(index=csv.index[outliers], axis=0, inplace=True)
 
 
-def split_tvt(dataset, validate=0.15, test=0):
-    # random = np.random.randint(1, 10000)
-    random = 800
+def split_tvt(dataset, validate=0, test=0.5):
+    random = np.random.randint(1, 10000)
+    # random = 800
     validate = dataset.sample(frac=validate, random_state=random)
     dataset = dataset.drop(validate.index)
 
@@ -134,18 +142,19 @@ def main(argv):
     test_data = pandas.read_csv(test_file)
     remove_outliers(train_data)
 
-    # train_data, va, te = split_tvt(train_data)
 
-    # plt.scatter(train_data.X, train_data.Y)
+    # train_data, va, test_data = split_tvt(train_data)
+
+    # plt.scatter(train_data.X, train_data.Y, edgecolors='red')
 
     reg = Regression()
     reg.gradient_descent(train_data.X, train_data.Y, validation_data=None)
-    # reg.plot_function(0, 0.4)
+    # reg.plot_function(0, 0.5)
 
-    # rmse = reg.root_mean_squared_error(te.Y, reg.calc_function(te.X))
+    # rmse = reg.root_mean_squared_error(reg.calc_function(te.X), te.Y)
     # print(rmse)
 
-    rmse = reg.root_mean_squared_error(test_data.Y, reg.calc_function(test_data.X))
+    rmse = reg.root_mean_squared_error(reg.calc_function(test_data.X), test_data.Y)
     print(rmse)
 
     # plt.show()
